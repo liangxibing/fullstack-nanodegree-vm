366a9a8058983c59f2554c4d893fdb55c58e0491
liangxibing <simonlaung@gmail.com>
2016-11-22 21:26:39 +1300

    Modify the GetAllPosts and AddPost functions in the forumdb.py file to use the database

--- vagrant/forum/forumdb.py
+++ vagrant/forum/forumdb.py
@@ -2,7 +2,7 @@
 # Database access functions for the web forum.
 # 
 
-import time
+import psycopg2
 
 ## Database connection
 DB = []
@@ -16,8 +16,12 @@ def GetAllPosts():
       pointing to the post content, and 'time' key pointing to the time
       it was posted.
     '''
+    conn = psycopg2.connect("dbname=forum");
+    cur = conn.cursor()
+    cur.execute("select content, time from posts order by time desc")
+    DB = cur.fetchall()
     posts = [{'content': str(row[1]), 'time': str(row[0])} for row in DB]
-    posts.sort(key=lambda row: row['time'], reverse=True)
+    conn.close()
     return posts
 
 ## Add a post to the database.
@@ -27,5 +31,8 @@ def AddPost(content):
     Args:
       content: The text content of the new post.
     '''
-    t = time.strftime('%c', time.localtime())
-    DB.append((t, content))
+    conn = psycopg2.connect("dbname=forum");
+    cur = conn.cursor()
+    cur.execute("insert into posts(content) values('%s')" % (content))
+    conn.commit()
+    conn.close()
